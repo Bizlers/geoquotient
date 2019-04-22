7c3b69f6ed73d8e41123e4afceb6be33e67b865a
PawanDalal <d.pawan05@gmail.com>
2019-04-22 16:17:10 +0530

    FIXED plugin directory not accessible in production distribution.

--- geo-discovery/src/main/java/com/bizlers/geoq/discovery/config/Pf4JConfiguration.java
+++ geo-discovery/src/main/java/com/bizlers/geoq/discovery/config/Pf4JConfiguration.java
@@ -1,28 +1,54 @@
 package com.bizlers.geoq.discovery.config;
 
 import com.bizlers.geoq.discovery.DatasetPreloader;
+import com.jcabi.log.Logger;
 import org.pf4j.spring.SpringPluginManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.IOException;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class Pf4JConfiguration {
 
+	private static final String SEP = File.separator;
+
 	@Autowired
-	ResourceLoader resourceLoader;
+	private ResourceLoader resourceLoader;
+
+	@Value("${pf4j.pluginsDir:#{null}}")
+	private String pluginsDir;
 
 	@Bean
 	public SpringPluginManager pluginManager() throws IOException {
-		File pluginsRoot = ResourceUtils.getFile("classpath:plugins");
+		File pluginsRoot;
+		if (pluginsDir != null) {
+			pluginsRoot = new File(pluginsDir);
+			Logger.debug(this, "Using pluginRoot from application.properties");
+		} else {
+			Resource resource = resourceLoader.getResource("classpath:plugins");
+			if (resource.exists() && resource.isFile()) {
+				pluginsRoot = resource.getFile();
+				Logger.debug(this, "Using pluginRoot from classpath");
+			} else {
+				File exeRoot = new File(System.getProperty("user.dir")).getParentFile();
+				pluginsRoot = new File(exeRoot + SEP + "lib" + SEP + "plugins");
+				Logger.debug(this, "Using pluginRoot from: %s", pluginsRoot);
+			}
+			Logger.debug(this, "pluginRoot: " + resource);
+		}
 		if (pluginsRoot.exists() && pluginsRoot.isDirectory()) {
 			return new SpringPluginManager(pluginsRoot.toPath());
+		} else {
+			Logger.warn(this, "plugins directory '%s' does not exist.", pluginsRoot);
 		}
 		return new SpringPluginManager();
 	}
